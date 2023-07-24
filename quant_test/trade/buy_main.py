8e410c6c2b1e3fa108b4321a291c8592ef95bfb1
ironbox1995 <1289318755@qq.com>
2023-07-24 09:27:38 +0800

    实盘逻辑

--- quant_test/trade/buy_main.py
+++ quant_test/trade/buy_main.py
@@ -3,5 +3,4 @@ from trade.place_order_main import *
 
 # 周一早上执行这个
 if __name__ == "__main__":
-    file_path = "实盘日志.txt"
     run_strategy_buy(file_path)
--- quant_test/trade/script/buy_script.py
+++ quant_test/trade/script/buy_script.py
@@ -2,15 +2,16 @@
 from trade.script.dingding_message import *
 from trade.script.script_utils import *
 
-# 每天早上执行这个
+
+# 周一早上执行这个
 if __name__ == "__main__":
-    file_path = "/trade/buy_main.py"
+    file_path = r"F:\quantitative_trading_dev_test\quant_test\trade\buy_main.py"
     try:
         if first_workday_in_period():  # 判断是周一且不止有一个交易日
             execute_script_in_virtualenv(file_path)
             send_dingding("交易播报：买入成功！具体交易细节见日志。")
         else:
-            send_dingding("今天不符合买入日条件。")
+            send_dingding("交易播报：今天不符合买入日条件。")
     except Exception as e:
         print(e)
         send_dingding("交易播报：买入失败！请详细查看后台了解具体原因！")
