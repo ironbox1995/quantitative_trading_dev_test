4b063a09cc503c27537df1291b8ae5760e9cdc9d
ironbox1995 <ironbox1995@hotmail.com>
2023-11-02 11:00:40 +0800

    【测试环境】 修改：整理配置文件

--- quant_test/backtest/back_test_config.py
+++ quant_test/backtest/back_test_config.py
@@ -2,16 +2,18 @@
 
 from utils import *
 
+# ==========策略配置==========
 strategy_li = ["小市值策略_分箱优化1", "小市值策略"]
 # strategy_li = ["小市值策略"]
 period_type_li = ['W']
 select_stock_num_li = [3]
 date_start = '2010-01-01'
 date_end = get_current_date()
+
+# ==========择时配置==========
 # pick_time_li = ["双均线择时", "MICD择时", "SROC择时", "ENV择时", "MTM择时", "DPO择时", "T3择时", "BBI择时", "PMO择时",
 #                 "PO择时", "RSIH择时", "WMA择时", "TMA择时", "MACD择时", "KDJ择时", "ARRON择时", "无择时"]
 pick_time_li = ["WMA择时", "无择时"]
-
 # 无创业无科创前提下：
 pick_time_mtd_dct = {"小市值策略": "无择时",  # DPO择时
                      "小市值策略_分箱优化1": "WMA择时",
@@ -24,5 +26,7 @@ pick_time_mtd_dct = {"小市值策略": "无择时",  # DPO择时
                      "换手率筛选多因子排序策略": "MTM择时",
                      "非高价股选股策略": "双均线择时"
                      }
+
+# ==========Q学习配置==========
 ALPHA = 0.9
 eps = -1
--- quant_test/trade/trade_config.py
+++ quant_test/trade/trade_config.py
@@ -1,26 +1,12 @@
 # -*- coding: utf-8 -*-
-# strategy_li = ["小市值策略", "小市值策略_量价优化1"]
+# ==========策略配置==========
 strategy_part_dct = {"小市值策略": 0.5, "小市值策略_分箱优化1": 0.5}  # 策略及资金权重
 strategy_li = strategy_part_dct.keys()
-strategy_stop_loss_point_dct = {"小市值策略": 1, "小市值策略_分箱优化1": 1}
-draw_down_warning_point = 0.8
+period_type = 'W'  # 数据周期
+select_stock_num = 3  # 选股数量
 
-"""
-每天买入一天期逆回购：https://bbs.quantclass.cn/thread/3401
-后续可以考虑反弹下单之类的手段
-'131810.SZ' --一天期
-'131809.SZ' --四天期
-:return:
-"""
-buy_reverse_repo = True
-repo_code = '131809.SZ'
-
-period_type = 'W'  # 周期不完整时也会看做一个完整周期正常选股
-select_stock_num = 3
-
-pick_time_switch = True
-Q_strategy_li = ["小市值策略", "香农短线量价策略", "低价小市值策略", "多因子量价策略2", "低回撤单因子组合策略",
-               "均线偏离与流通市值策略", "小市值策略_量价优化1", "换手率筛选多因子排序策略", "非高价股选股策略"]
+# ==========择时配置==========
+pick_time_switch = True  # 择时开关
 # 无创业无科创前提下：
 pick_time_mtd_dct = {"小市值策略": "无择时",  # DPO择时
                      "小市值策略_分箱优化1": "WMA择时",
@@ -33,4 +19,22 @@ pick_time_mtd_dct = {"小市值策略": "无择时",  # DPO择时
                      "换手率筛选多因子排序策略": "MTM择时",
                      "非高价股选股策略": "双均线择时"
                      }
+
+# ==========逆回购配置==========
+"""
+每天买入一天期逆回购：https://bbs.quantclass.cn/thread/3401
+'131810.SZ' --一天期
+'131809.SZ' --四天期
+"""
+buy_reverse_repo = True
+repo_code = '131809.SZ'
+
+# ==========止损配置==========
+strategy_stop_loss_point_dct = {"小市值策略": 1, "小市值策略_分箱优化1": 1}
+draw_down_warning_point = 0.8
+
+# ==========Q学习配置==========
+Q_strategy_li = ["小市值策略", "香农短线量价策略", "低价小市值策略", "多因子量价策略2", "低回撤单因子组合策略",
+               "均线偏离与流通市值策略", "小市值策略_量价优化1", "换手率筛选多因子排序策略", "非高价股选股策略"]
+
 eps = -1
