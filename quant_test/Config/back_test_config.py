b00239bc9d86e53c45a5a9c3012936ee44025e53
ironbox1995 <1289318755@qq.com>
2023-11-05 11:25:25 +0800

    【测试环境】 修改：修改回测配置

--- quant_test/Config/back_test_config.py
+++ quant_test/Config/back_test_config.py
@@ -14,10 +14,10 @@ date_end = get_current_date()
 # ==========择时配置==========
 # pick_time_li = ["双均线择时", "MICD择时", "SROC择时", "ENV择时", "MTM择时", "DPO择时", "T3择时", "BBI择时", "PMO择时",
 #                 "PO择时", "RSIH择时", "WMA择时", "TMA择时", "MACD择时", "KDJ择时", "ARRON择时", "无择时"]
-pick_time_li = ["WMA择时", "无择时"]
+# pick_time_li = ["WMA择时", "无择时"]
 # 无创业无科创前提下：
 pick_time_mtd_dct = {"小市值策略": "无择时",  # DPO择时
-                     "小市值策略_分箱优化1": "WMA择时",  # WMA择时
+                     "小市值策略_分箱优化1": "WMA择时",
                      "小市值策略_分箱优化4": "无择时",
                      "小市值策略_量价优化1": "MTM择时",
                      "小市值策略_分箱优化5": "SROC择时",
