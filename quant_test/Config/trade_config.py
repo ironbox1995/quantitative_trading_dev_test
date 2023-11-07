828f50b19b636656dd9449caab298fd211334101
ironbox1995 <1289318755@qq.com>
2023-11-07 22:38:47 +0800

    【测试环境】 修改：修改止损阈值

--- quant_test/Config/trade_config.py
+++ quant_test/Config/trade_config.py
@@ -33,9 +33,9 @@ buy_reverse_repo = True
 repo_code = '131809.SZ'
 
 # ==========止损配置==========
-# strategy_stop_loss_point_dct = {"小市值策略": 1, "小市值策略_分箱优化1": 1}
-strategy_stop_loss_point_dct = {"{}_{}_选{}_{}".format("小市值策略无创业无科创", period_type, select_stock_num, "无择时"): -0.35,
-                                "{}_{}_选{}_{}".format("小市值策略_分箱优化1无创业无科创", period_type, select_stock_num, "WMA择时"): -0.25}
+# 以从2022年初的回测为准
+strategy_stop_loss_point_dct = {"{}_{}_选{}_{}".format("小市值策略无创业无科创", period_type, select_stock_num, "无择时"): -0.2,
+                                "{}_{}_选{}_{}".format("小市值策略_分箱优化1无创业无科创", period_type, select_stock_num, "WMA择时"): -0.15}
 draw_down_warning_point = 0.8
 
 # ==========Q学习配置==========
