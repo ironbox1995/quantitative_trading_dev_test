891dbf12785f8ab11bead8a7334ce94d44f2c0be
ironbox1995 <1289318755@qq.com>
2023-12-10 21:44:24 +0800

    【测试环境】 修改：添加归一化

--- predictor/FCN/data_loader.py
+++ predictor/FCN/data_loader.py
@@ -1,59 +1,96 @@
 import torch
 import pandas as pd
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
+import joblib  # 用于保存和加载规范化器
 
 from quant_test.Config.global_config import *
 from predictor.FCN.filter import *
 
 
-def build_stock_regression_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter):
+def normalize_data(X_train, X_test, method='standard'):
+    if method == 'standard':
+        scaler = StandardScaler()
+    elif method == 'minmax':
+        scaler = MinMaxScaler()
+    elif method == 'robust':
+        scaler = RobustScaler()
+    else:
+        raise ValueError("Invalid normalization method. Choose 'standard', 'minmax', or 'robust'.")
 
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return scaler, X_train_scaled, X_test_scaled
+
+
+def build_stock_regression_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter
+                                    , norm_method='standard'):
+    # 加载数据
     data_path = r"{}\data\historical\processed_data\all_stock_data_{}.pkl".format(project_path, period_type)
     df = pd.read_pickle(data_path)
+
     # 根据日期过滤数据
     df = df[(df['交易日期'] >= pd.to_datetime(start_date)) & (df['交易日期'] <= pd.to_datetime(end_date))]
     df = filters(df, data_filter)  # 过滤数据
-    df = df[feature_li + ["下周期涨跌幅"]]  # 只选取我们需要的特征，避免排除数据时过度排除
+    df = df[feature_li + ["下周期涨跌幅"]]  # 只选取我们需要的特征
 
+    # 删除缺失值
     df.dropna(axis=0, how="any", inplace=True)
     X = df[feature_li].values
     y = df["下周期涨跌幅"].values
 
-    # Splitting the dataset into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X.numpy(), y.numpy(), test_size=0.2, random_state=42)
-    X_train, X_test, y_train, y_test = map(torch.tensor, (X_train, X_test, y_train, y_test))
+    # 分割数据集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Convert the training and test sets into PyTorch datasets
+    # 规范化数据
+    scaler, X_train_scaled, X_test_scaled = normalize_data(X_train, X_test, method=norm_method)
+
+    # 如果指定了路径，保存规范化器
+    scaler_save_path = f'scaler_FCN_reg_{start_date}_{end_date}-{period_type}-{data_filter}'
+    if scaler_save_path:
+        joblib.dump(scaler, scaler_save_path)
+
+    # 转换为PyTorch张量
+    X_train, X_test, y_train, y_test = map(torch.tensor, (X_train_scaled, X_test_scaled, y_train, y_test))
+
+    # 创建PyTorch数据集
     train_dataset = TensorDataset(X_train.float(), y_train.float())
     test_dataset = TensorDataset(X_test.float(), y_test.float())
 
-    # Create data loaders for batching
+    # 创建数据加载器
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    return train_loader, test_loader
+    return train_loader, test_loader, scaler
 
 
 def build_prediction_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter):
     # 加载数据
     data_path = r"{}\data\historical\processed_data\all_stock_data_{}.pkl".format(project_path, period_type)
     df = pd.read_pickle(data_path)
+
     # 根据日期过滤数据
     df = df[(df['交易日期'] >= pd.to_datetime(start_date)) & (df['交易日期'] <= pd.to_datetime(end_date))]
     df = filters(df, data_filter)  # 过滤数据
-    df = df[feature_li]  # 只选取我们需要的特征，避免排除数据时过度排除
+    df = df[feature_li]  # 只选取我们需要的特征
 
     # 删除缺失值
     df.dropna(axis=0, how="any", inplace=True)
-
-    # 选择特征
     X = df[feature_li].values
 
-    # 将数据转换为 PyTorch 张量
-    X_tensor = torch.tensor(X).float()
+    # 加载保存的规范化器
+    scaler_load_path = f'scaler_FCN_reg_{start_date}_{end_date}-{period_type}-{data_filter}'
+    scaler = joblib.load(scaler_load_path)
 
-    # 创建 PyTorch 数据集
+    # 规范化数据
+    X_normalized = scaler.transform(X)
+
+    # 转换为PyTorch张量
+    X_tensor = torch.tensor(X_normalized).float()
+
+    # 创建PyTorch数据集
     prediction_dataset = TensorDataset(X_tensor)
 
     # 创建数据加载器
--- predictor/LSTM/data_loader.py
+++ predictor/LSTM/data_loader.py
@@ -3,20 +3,35 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import joblib  # 用于保存和加载规范化器
 
 from quant_test.Config.global_config import *
 from predictor.LSTM.filter import *
 
 
-def generate_sequences(df, feature_li, sequence_length):
+def normalize_dataframe(df, feature_li, scaler=None):
+    if scaler is None:
+        scaler = StandardScaler()
+        df[feature_li] = scaler.fit_transform(df[feature_li])
+    else:
+        df[feature_li] = scaler.transform(df[feature_li])
+    return df, scaler
+
+
+def generate_sequences(df, feature_li, target_col, sequence_length):
     sequences = []
     for i in range(len(df) - sequence_length):
-        sequence = df[feature_li].iloc[i:i + sequence_length]
+        if target_col:
+            sequence = df[feature_li + [target_col]].iloc[i:i + sequence_length]
+        else:
+            sequence = df[feature_li].iloc[i:i + sequence_length]
         sequences.append(sequence.values)
     return np.array(sequences)
 
 
-def build_lstm_stock_regression_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter, sequence_length):
+def build_lstm_stock_regression_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter
+                                         , sequence_length=20):
     # 加载数据
     data_path = r"{}\data\historical\processed_data\all_stock_data_{}.pkl".format(project_path, period_type)
     df = pd.read_pickle(data_path)
@@ -25,8 +40,14 @@ def build_lstm_stock_regression_data_set(feature_li, start_date, end_date, perio
     df = df[feature_li + ["下周期涨跌幅"]]  # 选择特定特征和目标列
     df.dropna(axis=0, how="any", inplace=True)
 
-    # 生成序列数据
-    sequences = generate_sequences(df, feature_li + ["下周期涨跌幅"], sequence_length)
+    # 规范化特征
+    scaler_save_path = f'scaler_LSTM_reg_{start_date}_{end_date}-{period_type}-{data_filter}'
+    df, scaler = normalize_dataframe(df, feature_li)
+    if scaler_save_path:
+        joblib.dump(scaler, scaler_save_path)
+
+    # 生成序列数据（包含目标列）
+    sequences = generate_sequences(df, feature_li, "下周期涨跌幅", sequence_length)
     X = sequences[:, :, :-1]
     y = sequences[:, -1, -1]
 
@@ -40,10 +61,11 @@ def build_lstm_stock_regression_data_set(feature_li, start_date, end_date, perio
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    return train_loader, test_loader
+    return train_loader, test_loader, scaler
 
 
-def build_lstm_prediction_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter, sequence_length):
+def build_lstm_prediction_data_set(feature_li, start_date, end_date, period_type, batch_size, data_filter
+                                   , sequence_length=20):
     # 加载数据
     data_path = r"{}\data\historical\processed_data\all_stock_data_{}.pkl".format(project_path, period_type)
     df = pd.read_pickle(data_path)
@@ -52,8 +74,13 @@ def build_lstm_prediction_data_set(feature_li, start_date, end_date, period_type
     df = df[feature_li]  # 仅选择特定特征
     df.dropna(axis=0, how="any", inplace=True)
 
-    # 生成序列数据
-    sequences = generate_sequences(df, feature_li, sequence_length)
+    # 加载保存的规范化器
+    scaler_load_path = f'scaler_LSTM_reg_{start_date}_{end_date}-{period_type}-{data_filter}'
+    scaler = joblib.load(scaler_load_path)
+    df, _ = normalize_dataframe(df, feature_li, scaler)
+
+    # 生成序列数据（仅特征）
+    sequences = generate_sequences(df, feature_li, None, sequence_length)
     X = sequences
 
     # 数据转换
